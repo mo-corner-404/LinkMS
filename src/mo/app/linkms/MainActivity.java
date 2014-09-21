064b6a84d0ce420944c940c84a74b53725b6bc4d
momomok <rumosmok@gmail.com>
2014-09-21 19:28:35 +0800

    Removing hardcoded URI and index position

--- src/mo/app/linkms/MainActivity.java
+++ src/mo/app/linkms/MainActivity.java
@@ -10,11 +10,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-          
         String originuri = getIntent().getDataString();
         Uri playuri = trimUrl(originuri);
         
-		final Intent playIntent = new Intent(Intent.ACTION_VIEW);
+        final Intent playIntent = new Intent(Intent.ACTION_VIEW);
 		playIntent.setData(playuri);
 		startActivity(playIntent);
 		finish();
@@ -22,10 +21,10 @@ public class MainActivity extends Activity {
     }
     
 	protected Uri trimUrl(String in) {
-		
-		in = in.substring(49);
-		
-		Uri returnuri = Uri.parse("monsterstrike-app://joingame/?join="+ in);
+		String prefix = "monsterstrike-app://joingame/?join=";
+		int index = in.indexOf("=") + 1;
+		in = in.substring(index);
+		Uri returnuri = Uri.parse(prefix + in);
 		return returnuri;
 	}
 }
